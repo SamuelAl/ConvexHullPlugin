3393bf894642c2455c8b6dc55bed17de6ff55258
einstenz <33717014+einstenz@users.noreply.github.com>
2020-05-25 16:20:53 +0100

    Input selection and error checking

--- ConvexHullPlugin/ConvexHullPluginCommand.cs
+++ ConvexHullPlugin/ConvexHullPluginCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Rhino;
 using Rhino.Commands;
 using Rhino.Geometry;
@@ -32,12 +33,58 @@ namespace ConvexHullPlugin
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
             // Select points
+            if(!SelectPoints(out List<Point3d> inputPoints))
+            {
+                return Result.Failure;
+            }
+
+            // Check for errors
+            if(!CheckErrors(inputPoints))
+            {
+                return Result.Failure;
+            }
 
             // Calculate the convex hull
 
             // Draw convex hull line
 
             return Result.Success;
+        }
+
+        private bool CheckErrors(List<Point3d> points)
+        {
+            if (points.Count < 3)
+            {
+                RhinoApp.WriteLine("Convex hull needs at least 3 points");
+                return false;
+            }
+            Point3d[] noDuplicates = Point3d.CullDuplicates(points, RhinoDoc.ActiveDoc.ModelAbsoluteTolerance);
+
+            points = new List<Point3d>(noDuplicates);
+            return true;
+
+        }
+
+        private bool SelectPoints(out List<Point3d> inputPoints)
+        {
+            var go = new GetObject();
+
+            go.GeometryFilter = Rhino.DocObjects.ObjectType.Point;
+            go.GetMultiple(1, 0);
+
+            if (go.CommandResult() != Result.Success)
+            {
+                inputPoints = null;
+                return false;
+            }
+
+            inputPoints = new List<Point3d>(go.ObjectCount);
+
+            for (int i = 0; i < go.ObjectCount; i++)
+            {
+                inputPoints.Add(go.Object(i).Point().Location);
+            }
+            return true;
         }
     }
 }
